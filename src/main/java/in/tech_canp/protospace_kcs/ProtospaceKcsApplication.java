4c9dd22663576eece62607ee8cec3726c8cc863f
hiraidekazuki <k.hiraide472@gmail.com>
2025-06-05 15:39:32 +0900

    first commit

---
+++ src/main/java/in/tech_canp/protospace_kcs/ProtospaceKcsApplication.java
@@ -0,0 +1,13 @@
+package in.tech_canp.protospace_kcs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProtospaceKcsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProtospaceKcsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/in/tech_canp/protospace_kcs/ProtospaceKcsApplicationTests.java
@@ -0,0 +1,13 @@
+package in.tech_canp.protospace_kcs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProtospaceKcsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
