226efd480dbb200273955d195052ed634274e778
Mong-5 <tanaakabell@gmail.com>
2025-06-09 10:35:09 +0900

    保存機能

--- src/main/java/in/tech_camp/protospace/controller/ProtoController.java
+++ src/main/java/in/tech_camp/protospace/controller/ProtoController.java
@@ -1,25 +1,51 @@
 package in.tech_camp.protospace.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
+import in.tech_camp.protospace.entity.ProtoEntity;
 import in.tech_camp.protospace.form.ProtoForm;
+import in.tech_camp.protospace.repository.ProtoRepository;
 
 @Controller
 public class ProtoController {
 
+    @Autowired
+    private ProtoRepository protoRepository;
+
     @GetMapping("/protos/new")
     public String showProtoNew(Model model) {
-        // 仮のデータをセット
+        // 空のフォームオブジェクトをセット
         ProtoForm dummyForm = new ProtoForm();
         dummyForm.setName("");
         dummyForm.setCatchcopy("");
         dummyForm.setConcept("");
-        dummyForm.setImage("s"); // 画像は仮の名前
+        dummyForm.setImage("");
 
         model.addAttribute("protoForm", dummyForm);
+        return "protos/new"; // new.htmlを表示
+    }
 
-        return "protos/new"; // new.html（テンプレート）を表示
+    @PostMapping("/protos")
+    public String createProto(@ModelAttribute("protoForm") ProtoForm protoForm) {
+        // FormからEntityに値をコピー
+        ProtoEntity proto = new ProtoEntity();
+        proto.setName(protoForm.getName());
+        proto.setCatchcopy(protoForm.getCatchcopy());
+        proto.setConcept(protoForm.getConcept());
+        proto.setImage(protoForm.getImage());
+
+        try {
+            protoRepository.insert(proto);  
+        } catch (Exception e) {
+            System.out.println("エラー：" + e);
+            return "redirect:/protos/new"; 
+        }
+
+        return "redirect:/"; 
     }
 }
--- src/main/java/in/tech_camp/protospace/repository/ProtoRepository.java
+++ src/main/java/in/tech_camp/protospace/repository/ProtoRepository.java
@@ -1,13 +1,19 @@
 package in.tech_camp.protospace.repository;
 
-import org.apache.ibatis.annotations.Insert;
+import java.util.List
+
+import  org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Options;
+import org.apache.ibatis.annotations.Select;
 
 import in.tech_camp.protospace.entity.ProtoEntity;
 
 @Mapper
 public interface ProtoRepository {
+
+  @Select("SELECT * FROM protos")
+  List<ProtoEntity> findAll();
 
   @Insert("INSERT INTO protos (name, text, image) VALUES (#{name}, #{text}, #{image})")
   @Options(useGeneratedKeys = true, keyProperty = "id")
