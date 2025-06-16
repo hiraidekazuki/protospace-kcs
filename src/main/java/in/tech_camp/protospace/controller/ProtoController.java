e8dbfd69bc483b95011815c496b83db49c6fa4fa
susami112 <s.susami096@gmail.com>
2025-06-16 09:21:06 +0900

    ユーザー詳細ページ実装

--- src/main/java/in/tech_camp/protospace/controller/ProtoController.java
+++ src/main/java/in/tech_camp/protospace/controller/ProtoController.java
@@ -3,65 +3,30 @@ package in.tech_camp.protospace.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import in.tech_camp.protospace.component.ImageUrl;
+import in.tech_camp.protospace.custom_user.CustomUserDetails;
 import in.tech_camp.protospace.entity.ProtoEntity;
-
 import in.tech_camp.protospace.repository.ProtoRepository;
 
 @Controller
 public class ProtoController {
 
-    private final CommentRepository commentRepository;
-
-    private final ImageUrl imageUrl;
     private final ProtoRepository protoRepository;
 
     @Autowired
-    public ProtoController(ImageUrl imageUrl, ProtoRepository protoRepository) {
-        this.imageUrl = imageUrl;
+    public ProtoController(ProtoRepository protoRepository) {
         this.protoRepository = protoRepository;
-        this.commentRepository = commentRepository;
     }
 
-     @GetMapping("/")
-    public String showIndex(Model model) {
+    @GetMapping("/")
+    public String showIndex(Model model, @AuthenticationPrincipal CustomUserDetails userDetails) {
         List<ProtoEntity> prototypes = protoRepository.findAll();
         model.addAttribute("prototypes", prototypes);
-
+        model.addAttribute("user", userDetails != null ? userDetails.getUserEntity() : null);
         return "protos/index";
     }
-
-    // 投稿作成処理
-    // @PostMapping("/protos")
-    // public String createProto(
-    //     @Valid @ModelAttribute("protoForm") ProtoForm protoForm,
-    //     BindingResult bindingResult,
-    //     Model model,
-    //     @AuthenticationPrincipal CustomUserDetails userDetails
-    // ) {
-    //     if (bindingResult.hasErrors()) {
-    //         return "protos/new";
-    //     }
-
-    //     ProtoEntity proto = new ProtoEntity();
-    //     proto.setName(protoForm.getName());
-    //     proto.setCatchcopy(protoForm.getCatchcopy());
-    //     proto.setConcept(protoForm.getConcept());
-    //     // 画像アップロード未実装のため、protoFormから直接取得
-    //     // proto.setImage(protoForm.getImage());
-    //     // proto.setUser_name(userDetails != null ? userDetails.getUsername() : "anonymous");
-
-    //     try {
-    //         protoRepository.insert(proto);
-    //     } catch (Exception e) {
-    //         System.out.println("エラー：" + e);
-    //         return "redirect:/protos/new";
-    //     }
-
-    //     return "redirect:/";
-    // }
 }
--- src/main/java/in/tech_camp/protospace/repository/UserRepository.java
+++ src/main/java/in/tech_camp/protospace/repository/UserRepository.java
@@ -1,6 +1,9 @@
 package in.tech_camp.protospace.repository;
 
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Result;
+import org.apache.ibatis.annotations.ResultMap;
+import org.apache.ibatis.annotations.Results;
 import org.apache.ibatis.annotations.Select;
 
 import in.tech_camp.protospace.entity.UserEntity;
@@ -9,5 +12,18 @@ import in.tech_camp.protospace.entity.UserEntity;
 public interface UserRepository {
 
     @Select("SELECT * FROM users WHERE email = #{email}")
+    @Results(id = "UserResultMap", value = {
+        @Result(property = "id", column = "id"),
+        @Result(property = "email", column = "email"),
+        @Result(property = "password", column = "password"),
+        @Result(property = "name", column = "name"),
+        @Result(property = "profile", column = "profile"),
+        @Result(property = "groupName", column = "group_name"),
+        @Result(property = "post", column = "post")
+    })
     UserEntity findByEmail(String email);
+
+    @Select("SELECT * FROM users WHERE id = #{id}")
+    @ResultMap("UserResultMap")
+    UserEntity findById(Long id);
 }
