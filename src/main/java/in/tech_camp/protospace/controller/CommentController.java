f856e41da1e829e076ccf3e46f53a199dc064ca5
hiraidekazuki <k.hiraide472@gmail.com>
2025-06-17 13:19:34 +0900

    コメントコントローラーの作成

---
+++ src/main/java/in/tech_camp/protospace/controller/CommentController.java
@@ -0,0 +1,61 @@
+package in.tech_camp.protospace.controller;
+
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import in.tech_camp.protospace.custom_user.CustomUserDetails;
+import in.tech_camp.protospace.entity.CommentEntity;
+import in.tech_camp.protospace.entity.ProtoEntity;
+import in.tech_camp.protospace.form.CommentForm;
+import in.tech_camp.protospace.repository.CommentRepository;
+import in.tech_camp.protospace.repository.ProtoRepository;
+import in.tech_camp.protospace.repository.UserRepository;
+import jakarta.validation.Valid;
+import lombok.AllArgsConstructor;
+
+@Controller
+@AllArgsConstructor
+public class CommentController {
+
+    private final CommentRepository commentRepository;
+    private final UserRepository userRepository;
+    private final ProtoRepository protoRepository;
+
+    @PostMapping("/proto/{protoId}/comment")
+    public String createComment(@PathVariable("protoId") Integer protoId,
+                                @ModelAttribute("commentForm") @Valid CommentForm commentForm,
+                                BindingResult result,
+                                @AuthenticationPrincipal CustomUserDetails currentUser,
+                                Model model) {
+
+        ProtoEntity proto = protoRepository.findById(protoId);
+
+        if (result.hasErrors()) {
+            model.addAttribute("errorMessages", result.getAllErrors());
+            model.addAttribute("proto", proto);
+            model.addAttribute("commentForm", commentForm);
+            return "protos/detail";
+        }
+
+        CommentEntity comment = new CommentEntity();
+        comment.setText(commentForm.getText());
+        comment.setProto(proto);
+        comment.setUser(userRepository.findById(currentUser.getId()));
+
+        try {
+            commentRepository.insert(comment);
+        } catch (Exception e) {
+            model.addAttribute("proto", proto);
+            model.addAttribute("commentForm", commentForm);
+            System.out.println("エラー：" + e);
+            return "protos/detail";
+        }
+
+        return "redirect:/proto/" + protoId;
+    }
+}
