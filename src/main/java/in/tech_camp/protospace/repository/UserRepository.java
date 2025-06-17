d3db17f24cbf8b9de62512ed5fa332d91f586f64
hiraidekazuki <k.hiraide472@gmail.com>
2025-06-17 11:38:52 +0900

    詳細ページの所属の表示

--- src/main/java/in/tech_camp/protospace/repository/UserRepository.java
+++ src/main/java/in/tech_camp/protospace/repository/UserRepository.java
@@ -5,6 +5,8 @@ import java.util.List;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Options;
+import org.apache.ibatis.annotations.Result;
+import org.apache.ibatis.annotations.Results;
 import org.apache.ibatis.annotations.Select;
 
 import in.tech_camp.protospace.entity.UserEntity;
@@ -13,13 +15,19 @@ import in.tech_camp.protospace.entity.UserEntity;
 public interface UserRepository {
 
     @Select("SELECT * FROM users WHERE id = #{id}")
+    @Results({
+        @Result(property = "groupName", column = "group_name")
+    })
     UserEntity findById(Long id);
 
-    @Select("SELECT * FROM users WHERE email = #{email}") //ログイン時にそのメールアドレスのユーザーが存在するかの確認
+    @Select("SELECT * FROM users WHERE email = #{email}")
+    @Results({
+        @Result(property = "groupName", column = "group_name")
+    })
     UserEntity findByEmail(String email);
 
     @Select("SELECT COUNT(*) > 0 FROM users WHERE email = #{email}")
-    boolean existsByEmail(String email);  // ← 追加
+    boolean existsByEmail(String email);
 
     @Insert("""
         INSERT INTO users (email, password, name, profile, group_name, post)
@@ -29,5 +37,8 @@ public interface UserRepository {
     void insert(UserEntity user);
 
     @Select("SELECT * FROM users")
+    @Results({
+        @Result(property = "groupName", column = "group_name")
+    })
     List<UserEntity> findAll();
 }
