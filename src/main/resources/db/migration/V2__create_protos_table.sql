0e5d6faba7422ed8cb116e0fe8850f2208ec03a2
hiraidekazuki <k.hiraide472@gmail.com>
2025-06-17 11:07:45 +0900

    マイグレーションエラーの解消(仮)

--- src/main/resources/db/migration/V2__create_protos_table.sql
+++ src/main/resources/db/migration/V2__create_protos_table.sql
@@ -1,12 +1,12 @@
-CREATE TABLE protos (
-    id SERIAL PRIMARY KEY,
-    name TEXT NOT NULL,
-    catchcopy TEXT NOT NULL,
-    concept TEXT NOT NULL,
-    image TEXT,
-    user_id INTEGER NOT NULL,
-    CONSTRAINT fk_user
-        FOREIGN KEY (user_id)
-        REFERENCES users(id)
-        ON DELETE CASCADE
-);
+-- CREATE TABLE protos (
+--     id SERIAL PRIMARY KEY,
+--     name TEXT NOT NULL,
+--     catchcopy TEXT NOT NULL,
+--     concept TEXT NOT NULL,
+--     image TEXT,
+--     user_id INTEGER NOT NULL,
+--     CONSTRAINT fk_user
+--         FOREIGN KEY (user_id)
+--         REFERENCES users(id)
+--         ON DELETE CASCADE
+-- );
