3910acb2a3f43dcb74e2b27c6edade41a086bb25
hiraidekazuki <k.hiraide472@gmail.com>
2025-06-17 16:14:26 +0900

    コメントエンティティの作成

--- src/main/java/in/tech_camp/protospace/entity/CommentEntity.java
+++ src/main/java/in/tech_camp/protospace/entity/CommentEntity.java
@@ -1,14 +1,11 @@
 package in.tech_camp.protospace.entity;
 
-// 仮置き
-
 import lombok.Data;
 
 @Data
 public class CommentEntity {
-    private Integer id;
-    private String content;     // コメント内容
-    private String createdAt;   // 作成日時（文字列でも良いしDate型でもOK）
-    private Integer userId;     // コメントしたユーザーID
-    private Integer protoId;    // コメント対象のプロトタイプID（もしあれば）
+  private Integer id;
+  private String text;
+  private UserEntity user;
+  private ProtoEntity proto;
 }
